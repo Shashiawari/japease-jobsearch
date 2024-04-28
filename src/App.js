cf48bb246c62aa8f640c61a518f3afdaa6b992f6
Shashiawari <109402358+Shashiawari@users.noreply.github.com>
2024-04-28 10:00:47 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import Culture from "./components/culture/culture";
 import About from "./components/About/About";
 import SearchPage from "./components/search/Search";
 import NoPage from "./NoPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/japease2">
         <Routes>
-          <Route index element={<Main/>}></Route>
-          <Route path="/main" element={<Main/>}></Route>
-          <Route path="/home" element={<Home/>}></Route>
-          <Route path="/about" element={<About/>}></Route>
-          <Route path="/culture" element={<Culture/>}></Route>
-          <Route path="/search" element={<SearchPage/>}></Route>
-          <Route path="*" element={<NoPage/>}></Route>
+          <Route index element={<Main />}></Route>
+          <Route path="/main" element={<Main />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/culture" element={<Culture />}></Route>
+          <Route path="/search" element={<SearchPage />}></Route>
+          <Route path="*" element={<NoPage />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
