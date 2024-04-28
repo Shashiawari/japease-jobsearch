2f85fe624fd04f89e148dd8f2b4652a6e8a644a0
Shashiawari <109402358+Shashiawari@users.noreply.github.com>
2024-04-28 12:45:50 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { IntlProvider } from "react-intl";
-import messages from "./components/home/messages";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-ReactDOM.render(
-  <IntlProvider locale="en" messages={messages["en"]}>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </IntlProvider>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
